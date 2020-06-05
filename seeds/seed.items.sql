0dbf44de40bb324477665d4bd8bc1ea8e9ff68eb
Yajaira Flynn-Corbin <yary.ba26@gmail.com>
2020-06-05 00:39:02 -0400

    set up

---
+++ seeds/seed.items.sql
@@ -0,0 +1,13 @@
+INSERT INTO items (pace, content, user_id)
+VALUES
+  (8.50, 'I had to poo', 1),
+  (7.50, 'I had to poo', 2),
+(9.50, 'I had to poo', 3),
+(6.50, 'I had to poo', 4),
+  (8.50, 'I had to poo', 5),
+  (11.50, 'I had to poo', 1),
+  (8.50, 'I had to poo', 2),
+  (9.50, 'I had to poo', 3),
+  (8.50, 'I had to poo', 4),
+  (10.50, 'I had to poo', 5);
+  
--- src/config.js
+++ src/config.js
@@ -1,4 +1,9 @@
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-}
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgres://dunder_mifflin@localhost:5432/pace",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgres://dunder_mifflin@localhost:5432/pace_test",
+};
